72be0a587fbf793105a9377b970ab2383d59e9a5
Ash Pirnia <apirnia@amazon.com>
2020-09-24 00:07:40 +1000

    Added a clock, a link, and a button (redirecting to an image in s3) to the first page.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component }  from 'react';
 import logo from './logo.svg';
+import Clock from './Clock';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p> Life goes on. </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Under construction!
-        </p>
+        <Clock />
       </header>
+      <footer className="App-footer">
+        <p style={{color: "red"}}>
+          Do NOT push this <Button variant="danger" onClick={handleClick}>button</Button>
+        </p>
+        <p>
+          Running <a href="https://github.com/ashpirnia/apui">LSIM-6.8</a>
+        </p>
+      </footer>
     </div>
   );
+}
+
+function handleClick() {
+  window.location.assign("https://images-for-ash-website.s3-ap-southeast-2.amazonaws.com/apui-imgs/IMG_20180714_085534.jpg");
 }
 
 export default App;
